bc4cf07d285b5cb25015f865b8090c6701d7b04a
Okpeyemi <maqsoud.tawaliou@epitech.eu>
2025-03-09 09:47:47 +0100

    feat: update to responsive

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <div>
       <Header className="px-[10%] max-lg:px-[3%] py-3" />
       </div>
-      <Hero className="w-[80%] max-lg:w-[95%] mx-auto py-54" />
+      <Hero className="w-[80%] max-lg:w-[95%] mx-auto py-54 max-lg:py-40" />
       <div className="w-full bg-background">
         <News className="w-[80%] max-lg:w-[95%] mx-auto py-20" />
         <About className="xl:w-[80%] lg:w-[95%] mx-auto py-20" />
--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -7,11 +7,11 @@ interface props {
 const Hero: React.FC<props> = ({ className }) => {
   return (
     <div className={`${className} flex flex-col text-white space-y-10 max-lg:text-center`}>
-      <div className="flex flex-col lg:w-2/3">
-        <span className="font-poppins text-7xl max-md:text-5xl">Receuil de</span>
-        <span className="font-kablammo text-7xl max-md:text-5xl">BD Africain</span>
+      <div className="flex lg:flex-col max-lg:space-x-5 lg:w-2/3">
+        <span className="font-poppins text-7xl max-sm:text-2xl max-md:text-3xl max-lg:text-4xl max-md:text-start">Receuil de</span>
+        <span className="font-kablammo text-7xl max-sm:text-2xl max-md:text-3xl max-lg:text-4xl max-md:text-start">BD Africain</span>
       </div>
-      <p className="lg:w-1/2 text-justify">
+      <p className="lg:w-1/2 max-md:text-sm text-justify">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium
         quibusdam ipsum doloribus, adipisci vitae ipsa repudiandae amet
         molestiae. Alias, atque expedita ad harum esse vero dolorem ipsam
